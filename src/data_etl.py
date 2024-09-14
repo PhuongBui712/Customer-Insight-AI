9576c3474f74256ae3f63ff992bb4a1968046c5a
PhuongBui712 <Buitanphuong712@gmail.com>
2024-09-14 21:07:06 +0700

    feat: update data etl v4

--- src/data_etl.py
+++ src/data_etl.py
@@ -21,7 +21,7 @@ def filter_pages(name: str, patterns: List):
     return False
 
 
-def update_page(schema_path: str, page_filter_patterns: Optional[List[str]] = None):
+def update_page(schema_path: str, default_last_check: int = 30, page_filter_patterns: Optional[List[str]] = None):
     # check exitent schema
     page_schema = {}
     if os.path.exists(schema_path):
@@ -59,7 +59,7 @@ def update_page(schema_path: str, page_filter_patterns: Optional[List[str]] = No
         page_schema[k] = {}
         page_schema[k]['page_access_token'] = pages[k]['page_access_token']
         # set up for conversations, messages scheme
-        page_schema[k]['last_check'] = get_day_before(30)
+        page_schema[k]['last_check'] = get_day_before(default_last_check)
         page_schema[k]['conversations'] = {}
     
     return page_schema
@@ -153,9 +153,9 @@ def update_messages(conversations: dict, new_check: int):
 
 
 # --------------------- Get Messages main process ---------------------
-def pancake_etl(schema_path: str, filter_patterns: Optional[List] = None):
+def pancake_etl(schema_path: str, default_last_check: int = 30, filter_patterns: Optional[List] = None):
     # 1. Update pages
-    page_schema = update_page(schema_path, filter_patterns)
+    page_schema = update_page(schema_path, default_last_check, filter_patterns)
 
     # 2. Update conversations
     new_check = int(time.time())
@@ -188,7 +188,7 @@ def setup_data_directory(root_dir: str, sub_dir_list: List[str]) -> None:
 
 # --------------------- ETL tasks ---------------------
 # task: remove old data at beginning of a day
-def remove_old_data(table_path: str, num_days_before: int):
+def remove_old_data(table_path: str, sheet_idx: int, queue_path: str, num_days_before: int):
     # check data has been collected
     if not os.path.exists(table_path):
         return
@@ -198,61 +198,68 @@ def remove_old_data(table_path: str, num_days_before: int):
     if now.hour != 0 and now.minute > 30:
         return
     
-    # remove
-    message_df = load_worksheet(0)
     date_bound = get_day_before(num_days_before, return_type='date')
+    
+    # remove data on queue
+    if os.path.exists(queue_path):
+        queue_messsages = load_json(queue_path)
+        queue_messsages = [
+            m for m in queue_messsages 
+            if string_to_unix_second(m['inserted_at']) > int(date_bound.timestamp())
+        ]
+        save_json(queue_path, queue_messsages)
+    
+    # remove data on sheet
+    message_df = load_worksheet(sheet_idx=sheet_idx)
+    message_df = pd.to_datetime(message_df['inserted_at'], format='%Y-%m-%d %H:%M:%S')
     message_df = message_df.loc[message_df['inserted_at'] > date_bound]
 
     # udpate worksheet
-    update_worksheet(message_df, sheet_idx=0)
+    update_worksheet(message_df, sheet_idx=sheet_idx)
 
     # update local sheet
     message_df.to_csv(table_path, index=False)
 
 
 # task 2: update new data (pages, conversations, messages)
-def update_new_data(config: dict):
+def update_new_data(config: dict) -> List[dict]:
     schema = os.path.join(PROJECT_DIRECTORY, config['schema'])
+    default_last_check = config['default-last-check']
     filter_patterns = config['filter-page-keywords']
-    messages = pancake_etl(schema, filter_patterns)
+    messages = pancake_etl(schema, default_last_check, filter_patterns)
+    messages = [m for m in messages if m['from'] == 'customer']
 
     # store messages lists
-    total_message_path = os.path.join(PROJECT_DIRECTORY, config['total-message'])
-    total_messages = messages.copy()
-    if os.path.exists(total_message_path):
-        total_messages = load_json(total_message_path) + messages
-
-    save_json(total_message_path, total_messages)
-
-    return messages, total_messages
-
-
-# task 3: load data to be analysed
-def load_analyse_data(config: dict, messages: List[str], total_messages: List[str]):
-    # load messages needed to analyse
-    message_table_path = os.path.join(PROJECT_DIRECTORY, config['message-table'])
-    error_path = os.path.join(PROJECT_DIRECTORY, config['error-message'])
-
-    # load total messages if not analysing yet
-    if not os.path.exists(message_table_path):
-        messages = [m for m in total_messages if m['from'] == 'customer']
-
-    # load error messages if exist
-    if os.path.exists(error_path):
-        messages += load_json(error_path)
+    queue_message_path = os.path.join(PROJECT_DIRECTORY, config['queue-message'])
+    if os.path.exists(queue_message_path):
+        messages = load_json(queue_message_path) + messages
 
     return messages
 
 
+# task 3: load data to be analysed
+def load_analyse_data(config: dict, messages: List[str], num_sample: int = 500):
+    # load messages needed to analyse
+    analyse_messages = messages[-num_sample:]
+
+    # store queue messages
+    queue_path = os.path.join(PROJECT_DIRECTORY, config['queue-message'])
+    queue_messages = messages[:-num_sample]
+    save_json(queue_path, queue_messages)
+
+    return analyse_messages
+
+
 # task 6: update tables
-def update_table(config: dict, processed_messages: List[dict], error_messages: Optional[List[dict]]):
+def update_table(config: dict, processed_messages: List[dict]):
     # update message df
-    message_sheet_idx = config['google-sheet']['message-sheet-iddx']
-    last_message_df = load_worksheet(message_sheet_idx)
+    message_sheet_idx = config['google-sheet']['message-sheet-idx']
+    last_message_sheet = load_worksheet(sheet_idx=message_sheet_idx)
+    last_message_df = sheet_to_df(last_message_sheet)
     new_message_df = create_dataframe(processed_messages)
     updated_message_df = pd.concat([last_message_df, new_message_df], axis=0)
 
-    update_worksheet(updated_message_df, message_sheet_idx, mode='replace')
+    update_worksheet(updated_message_df, sheet_idx=message_sheet_idx, mode='replace')
     updated_message_df.to_csv(os.path.join(PROJECT_DIRECTORY, config['message-table']), index=False)
 
     # update 2 stats sheet
@@ -260,14 +267,10 @@ def update_table(config: dict, processed_messages: List[dict], error_messages: O
     purpose_sheet_idx = config['google-sheet']['purpose-sheet-idx']
     user_df, purpose_df = analyse_data(updated_message_df)
 
-    update_worksheet(user_df, user_sheet_idx, mode='replace')
+    update_worksheet(user_df, sheet_idx=user_sheet_idx, mode='replace')
     user_df.to_csv(os.path.join(PROJECT_DIRECTORY, config['user-table']))
-    update_worksheet(purpose_df, purpose_sheet_idx, mode='replace')
+    update_worksheet(purpose_df, sheet_idx=purpose_sheet_idx, mode='replace')
     purpose_df.to_csv(os.path.join(PROJECT_DIRECTORY, config['purpose-table']))
-
-    # store failed messages
-    if error_messages:
-        save_json(os.path.join(PROJECT_DIRECTORY, config['error-message']))
     
 
 # complete ETL
@@ -283,13 +286,16 @@ def analyse_customer_message_pipeline():
     )
 
     # 2. Check new day & delete old data
-    remove_old_data(table_path=os.path.join(PROJECT_DIRECTORY, config['message-table']), num_days_before=90)
-    
+    message_table_path = os.path.join(PROJECT_DIRECTORY, config['message-table'])
+    queue_path = os.path.join(PROJECT_DIRECTORY, config['queue-message'])
+    message_sheet_idx = config['google-sheet']['message-sheet-idx']
+    remove_old_data(table_path=message_table_path, sheet_idx=message_sheet_idx, queue_path=queue_path, num_days_before=90)
+
     # 3. get new messages
-    messages, total_messages = update_new_data(config)
+    messages = update_new_data(config)
 
     # 4. load analyse messages
-    messages = load_analyse_data(config, messages, total_messages)
+    messages = load_analyse_data(config, messages, config['num-sample'])
 
     # 5. analysing
     processed_messages, error_messages = analyse_message_pipeline(
@@ -300,8 +306,14 @@ def analyse_customer_message_pipeline():
         provider=config['provider']
     )
 
-    # 6. update tables
-    update_table(config, processed_messages, error_messages)
+    # 6. store error messages to queue
+    if error_messages:
+        queue_message = load_json(queue_path) + error_messages
+        save_json(queue_path, queue_message)
+
+    # 7. update tables
+    if processed_messages:
+        update_table(config, processed_messages)
 
 
 if __name__ == '__main__':
