6e0b9dba706da56199cebd56551d56168346733a
PhuongBui712 <Buitanphuong712@gmail.com>
2025-01-27 18:00:28 +0700

    feat: add airflow dags

--- airflow/dags/etl_dags_v2.py
+++ airflow/dags/etl_dags_v2.py
@@ -1,28 +1,30 @@
+import os
+from datetime import datetime
+
+import pendulum
+
 from airflow import DAG
 from airflow.decorators import task
-from datetime import datetime
-import pendulum
-import os
 
 # Get the absolute path of the project root
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 # Path to the virtual environment
-venv_path = os.path.join(project_root, '.venv')
-python_path = os.path.join(venv_path, 'bin/python')
+venv_path = os.path.join(project_root, ".venv")
+python_path = os.path.join(venv_path, "bin/python")
 
 # Path to the data_etl.py file
-etl_file_path = os.path.join(project_root, 'src', 'data_etl.py')
+etl_file_path = os.path.join(project_root, "src", "data_etl.py")
 
 # timezone
 timezone = pendulum.timezone("Asia/Bangkok")
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': pendulum.datetime(2024, 1, 1, tz=timezone),
-    'email_on_failure': False,
-    'email_on_retry': False,
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": pendulum.datetime(2024, 1, 1, tz=timezone),
+    "email_on_failure": False,
+    "email_on_retry": False,
 }
 
 with DAG(
@@ -32,16 +34,17 @@ with DAG(
     catchup=False,
     max_active_runs=1,
 ) as dag:
-    @task.external_python(task_id='data_etl', python=python_path)
+
+    @task.external_python(task_id="data_etl", python=python_path)
     def data_etl(project_root_path):
         import os
         import sys
-        
+
         # Add project root to system path
         sys.path.append(project_root_path)
-        
+
         # Add src directory to system path
-        src_path = os.path.join(project_root_path, 'src')
+        src_path = os.path.join(project_root_path, "src")
         sys.path.append(src_path)
 
         # import
